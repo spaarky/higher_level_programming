d1ad81d8c59938bc68ae461cacceceb730566ddf
Spark4545 <lerouxpaulhenri@gmail.com>
2023-05-30 14:14:59 +0200

    indent probleme solved

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     size = len(userin)
     print('{:d} {:s}{:s}'.
           format(size,
-                  'arguments' if (size) is not 1 else 'argument',
-                  '.' if (size) is 0 else ':'))
+                 'arguments' if (size) is not 1 else 'argument',
+                 '.' if (size) is 0 else ':'))
     for index, arg in enumerate(userin):
         print('{:d}: {:s}'.format(index + 1, arg))
