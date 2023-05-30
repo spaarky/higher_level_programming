1d12478026fd7f532cda4f376fe17994544442d5
Spark4545 <lerouxpaulhenri@gmail.com>
2023-05-30 14:18:58 +0200

    nez task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
-    size = len(userin)
-    print('{:d} {:s}{:s}'.
-          format(size,
-                 'arguments' if (size) is not 1 else 'argument',
-                 '.' if (size) is 0 else ':'))
-    for index, arg in enumerate(userin):
-        print('{:d}: {:s}'.format(index + 1, arg))
+    import sys
+    number = len(sys.argv)
+
+    if number == 1:
+        print("{} arguments.".format(number - 1))
+    elif number == 2:
+        print("{} argument:".format(number - 1))
+    else:
+        print("{} arguments:".format(number - 1))
+
+    for i in range(1, number):
+        print("{}: {}".format(i, sys.argv[i]))
