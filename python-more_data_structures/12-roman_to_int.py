99638bf11041cba2179905e11960b2d3e7231f07
Spark4545 <lerouxpaulhenri@gmail.com>
2023-06-08 10:00:49 +0200

    why does it work

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     if roman_string == "":
         return 0
     num = 0
-    romanDic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i, j in zip(roman_string, roman_string[1:]):
-        if i not in romanDic.keys():
+        if i not in dic.keys():
             return 0
-        elif romanDic[i] >= romanDic[j]:
-            num += romanDic[i]
+        elif dic[i] >= dic[j]:
+            num += dic[i]
         else:
-            num -= romanDic[i]
-    num += romanDic[roman_string[-1]]
+            num -= dic[i]
+    num += dic[roman_string[-1]]
     return num
