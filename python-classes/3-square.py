cbcd36bfc6a7d355f32c1fc544c0d4301c237f52
Spark4545 <lerouxpaulhenri@gmail.com>
2023-06-12 12:26:57 +0200

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square with private attribute size and validate size
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of a side of the square
+
+    Functions:
+        __init__(self, size)
+        area(self)
+    """
+    def __init__(self, size=0):
+        """
+        Initialize square
+
+        Attribute:
+            __size (int): size of a side in the square, defaults to 0 if None
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Calculation of area of square
+
+        Returns:
+            area
+        """
+        return (self.__size)**2
