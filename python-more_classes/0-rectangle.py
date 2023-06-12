b83acce80a06200ee07f226b1f19e0e377507fc3
Spark4545 <lerouxpaulhenri@gmail.com>
2023-06-12 14:37:43 +0200

    init commit and task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Contains class Rectangle
+"""
+
+class Rectangle:
+    """
+    Defines empty class rectangle
+    """
+    def __init__(self):
+        pass
