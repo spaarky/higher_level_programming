e9633095571767191500b7df32adb59bcc0f1fda
Spark4545 <lerouxpaulhenri@gmail.com>
2023-06-23 16:49:45 +0200

    i dont get it why it doesnt work

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -16,16 +16,14 @@ Contains function that adds and saves to Python obj to JSON file; loads objects
 
 
 from sys import argv
-
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 try:
-    exists = load_from_json_file(filename)
+    existing_content = load_from_json_file(filename)
 except FileNotFoundError:
-    exists = []
+    existing_content = []
 
-save_to_json_file(exists + argv[1:], filename)
+save_to_json_file(existing_content + argv[1:], filename)
