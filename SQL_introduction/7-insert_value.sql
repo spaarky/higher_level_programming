5a85a7916b3d255b6c43bc6e7d8705d848023b12
Spark4545 <lerouxpaulhenri@gmail.com>
2023-07-24 02:20:28 +0200

    task 7 help

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert a new row of data in this table in this database
 
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, 'Holbereton school');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
