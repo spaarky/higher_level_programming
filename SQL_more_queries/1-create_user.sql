270f2d72235e5c965dd8a19d3adf6b9747f157fc
Spark4545 <lerouxpaulhenri@gmail.com>
2023-07-26 15:20:39 +0200

    task 1 n 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
--- creates the MySQL server user user_0d_1
---MySQL
+-- Creates a user user_0d_1@localhost
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- Grant all privileges to user_0d_1@localhost
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
