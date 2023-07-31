a875ac60a027d64684b30848fa7fca895b912e47
Spark4545 <lerouxpaulhenri@gmail.com>
2023-07-31 12:59:16 +0000

    task i wanna jump off a cliff

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@ parameters given to script: username, password, database
 """
 
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
