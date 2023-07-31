2347c713d6b7f286528fd1bea916bc2fd1fb128e
Spark4545 <lerouxpaulhenri@gmail.com>
2023-07-31 17:10:35 +0000

    task 1,2,3 and 4

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+return states starting with 'N'
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries ; match arg[4]
+    cursor = db.cursor()
+    sqlCmd = """SELECT *
+                FROM states
+                WHERE name=%s ORDER BY id ASC""".format(argv[4])
+    cursor.execute(sqlCmd, (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
